0ccc1fb024d1529b082d4f29ce65d44574d6e706
Max Sun <max.sun@yale.edu>
2019-05-15 04:55:17 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,10 @@
 
 def levitation_quiz
-	#your code here
+	loop do
+	  puts "What is the spell that enacts leviation?"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
+	end
 end
 
 
